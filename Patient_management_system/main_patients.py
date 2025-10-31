c459e5021f5b91dded8deb2083161ff5f311d07f
kitofficial <ankittiwariofficial92@gmail.com>
2025-11-01 01:48:33 +0530

    change 3

--- Patient_management_system/main_patients.py
+++ Patient_management_system/main_patients.py
@@ -1,7 +1,7 @@
 
 # To understand a simple hello world application using FastAPI
 
-from fastapi import FastAPI , Path, HTTPException
+from fastapi import FastAPI , Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -44,6 +44,31 @@ def view_patient(patient_id: str = Path(...,description = "Id of the patient in 
     
     # return {"message": "Patient not found"}  #return a message if no matching patient is found
     raise HTTPException(status_code=404, detail="Patient not found")  #return a 404 HTTP exception if no matching patient is found
+
+@app.get('/sort')
+def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
+
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail='Invalid order select between asc and desc')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+
+    
+
+
+                  
+
 
 
 
